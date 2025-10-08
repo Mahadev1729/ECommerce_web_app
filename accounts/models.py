57d677489283355ca5cccd9ac7243c70979c75bd
Mahadev1729 <devcampus17@gmail.com>
2025-10-08 22:10:00 +0530

    ui fix

--- accounts/models.py
+++ accounts/models.py
@@ -6,5 +6,5 @@ from base.models import BaseModel
 class Profile(BaseModel):
     user=models.OneToOneField(User,on_delete=models.CASCADE,related_name="profile")
     is_email_verified=models.BooleanField(default=False)
-    email_token=models.CharField(max_lenth=100,null=True,blak=True)
+    email_token=models.CharField(max_length=100,null=True,blank=True)
     profile_image=models.ImageField(upload_to="profile")
